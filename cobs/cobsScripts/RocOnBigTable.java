1e571b1d0839bc7efbd6f8fc427b22a7ea392778
kkreth <krethfamily@gmail.com>
2013-06-11 11:17:59 -0400

    The output for this file is now able to be parsed by R, allowing for data to be "flattened" in that large families will not weigh more heavily on results.

--- cobs/cobsScripts/RocOnBigTable.java
+++ cobs/cobsScripts/RocOnBigTable.java
@@ -5,7 +5,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import utils.ConfigReader;
 
@@ -15,82 +17,131 @@ import utils.ConfigReader;
  */
 public class RocOnBigTable
 {
-		public static void main(String[] args) throws Exception
+	public static void main(String[] args) throws Exception
+	{
+		for(String s: AbsoluteScoreVsAverageDistance.FILE_NAMES)
 		{
-			for(String s: AbsoluteScoreVsAverageDistance.FILE_NAMES)
-			{
-				writeROC(s,true);
-				writeROC(s, false);
-			}
-				
+			writeROC(s,true);
+			writeROC(s, false);
+		}
+		System.out.println("All files completed.");
+	}
+	
+	public static void writeROC(String fileSubString, boolean normalized) throws Exception
+	{
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getCleanroom() + File.separator + 
+						"bigSummaries" + File.separator + "big" + fileSubString + (normalized ? "normedLate" : "") 
+						+ ".txt")));
+		
+		File rocDir = new File(ConfigReader.getCleanroom() + File.separator + 
+				"roc" );
+		
+		rocDir.mkdirs();
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				rocDir.getAbsolutePath() + File.separator 
+				+ "_bigROC" + fileSubString +(normalized ? "normedLate" : "")+ "_" + 
+						".txt"));
+		
+		int numBelow50=0;
+		int numAbove50=0;
+		
+		writer.write("family\t" + "thisFamilyTotal\t" +
+					"PerPFAMUnit" + fileSubString +  "_prediction\t" + 
+					"PerPFAMUnit" + fileSubString +  "_numBelow50\t" + 
+					"PerPFAMUnit" + fileSubString +	"_numAbove50\t" +
+					fileSubString +  "_prediction\t" + 
+					fileSubString +  "_numBelow50\t" + 
+					fileSubString +	"_numAbove50\n");
+							
+		
+		reader.readLine();
+		//Looks like we need a re-usable List here
+		List<String> theReadLines = new ArrayList<String>();
+		for(String s= reader.readLine(); s != null; s = reader.readLine()){
+			theReadLines.add(s);
+		}
+			
+		
+		//Hopefully you can do this twice with Reader!  We will iterate through to get the PerPFAM counts necessary above
+		//And create a HashMap of that Data for use later
+		//With a HashMap of the below and a HashMap of the Above
+		HashMap<String,Integer> pFAMtotalHits = new HashMap<String, Integer>();
+		HashMap<String,Integer> pFAMsubTotal = new HashMap<String, Integer>();
+		HashMap<String,Integer> pFAMabove = new HashMap<String, Integer>();
+		
+		//This first read-through will create many "redundant" keys, but that won't be a problem, they have to be unique
+		//for(String s : theReadLines.subList( 1, theReadLines.size() )) 
+		for(String s : theReadLines)
+		{
+			String[] splits = s.split("\t");
+			String family = (splits[0]);
+			pFAMtotalHits.put(family, 0);
+			pFAMsubTotal.put(family, 0);
+			pFAMabove.put(family, 0);
 		}
 		
-		/*"AverageMcBASC_PNormalInitial", 
-		"AverageMcBASC", 
-		"COBS_UNCORRECTED",
-		"random_PNormalInitial",
-		"AverageConservationSum_PNormalInitial",
-		"AverageConservationSum",
-		"AverageMI_PNormalInitial",
-		"AverageMI"
-		 * 
-		 */
-		private static String annotateColumn(String fileSubString,boolean normalized)
+		//Now we will record the Total
+		for(String s : theReadLines)
 		{
-			StringTokenizer sToken = new StringTokenizer(fileSubString, "_");
-			
-			String firstToken = sToken.nextToken();
-			
-			String suffix = normalized ? "late" : "";
-			
-			if( ! sToken.hasMoreTokens())
-				return firstToken + "_" + suffix;
-			
-			return firstToken + "_" +  sToken.nextToken().replace("PNormalInitial", "early") + "_" + suffix;
+			String[] splits = s.split("\t");
+			String family = (splits[0]);
+			Integer currentTotal = pFAMtotalHits.get(family);
+			currentTotal++;
+			pFAMtotalHits.put(family, currentTotal);
 		}
 		
-		public static void writeROC(String fileSubString, boolean normalized) throws Exception
+		
+		for(String s : theReadLines)
 		{
+			String[] splits = s.split("\t");
 			
-			BufferedReader reader = new BufferedReader(new FileReader(new File(
-					ConfigReader.getCleanroom() + File.separator + 
-							"bigSummaries" + File.separator + "big" + fileSubString + (normalized ? "normedLate" : "") 
-							+ ".txt")));
+			String family = (splits[0]);
 			
-			File rocDir = new File(ConfigReader.getCleanroom() + File.separator + 
-					"roc" );
-			
-			rocDir.mkdirs();
-			
-			BufferedWriter writer = new BufferedWriter(new FileWriter(
-					rocDir.getAbsolutePath() + File.separator 
-					+ "bigROC" + fileSubString +(normalized ? "normedLate" : "")+ "_" + 
-							".txt"));
-			
-			int numBelow50=0;
-			int numAbove50=0;
-			
-			String prefix = annotateColumn(fileSubString, normalized);
-			writer.write( prefix +  "_prediction\t" + 
-						prefix +  "_numBelow50\t" + 
-						prefix +	"_numAbove50\n");
-			
-			reader.readLine();
-			
-			for(String s= reader.readLine(); s != null; s = reader.readLine())
-			{
-				String[] splits = s.split("\t");
-				if( Double.parseDouble(splits[4]) <= 50)
-					numBelow50++;
-				else
-					numAbove50++;
-				
-				writer.write( (numBelow50 + numAbove50) + "\t" );
-				writer.write( numBelow50 + "\t" );
-				writer.write( numAbove50+ "\n" );
+			//Now to track the running total
+			Integer subTotal = pFAMsubTotal.get(family);
+			subTotal++;
+			pFAMsubTotal.put(family, subTotal);
+		
+			if( Double.parseDouble(splits[4])  < 50){
+				numBelow50++;
 			}
+				
+			else{
+				numAbove50++;
+				Integer perFamilyAbove = pFAMabove.get(family);
+				perFamilyAbove++;
+				pFAMabove.put(family,perFamilyAbove);
+			}
+				
+				
+			//Now to carry the family name (without proper parsing...but good enough...forward)
+			writer.write(family + "\t");
 			
-			writer.flush(); writer.close();
-			reader.close();
+			//Now to always carry the total for the family through
+			writer.write(pFAMtotalHits.get(family) + "\t");
+			
+			//Now the three additional columns we require, one of which is "derived", which we use here as a sort of
+			//check digit
+			int targetPFAMhighWaterMark = pFAMsubTotal.get(family);
+			writer.write(targetPFAMhighWaterMark + "\t");
+			int targetPFAMaboveCount = pFAMabove.get(family);
+			writer.write(targetPFAMaboveCount + "\t");
+			int whatIsLeftForThisPFAMToAchieve = targetPFAMhighWaterMark - targetPFAMaboveCount;
+			writer.write(whatIsLeftForThisPFAMToAchieve + "\t");
+			
+			writer.write( (numBelow50 + numAbove50) + "\t" );
+
+			writer.write( numBelow50 + "\t" );
+			writer.write( numAbove50+ "\n" );
+			writer.flush();
 		}
+		
+		writer.flush(); writer.close();
+		reader.close();
+	}
+	
+	
 }
